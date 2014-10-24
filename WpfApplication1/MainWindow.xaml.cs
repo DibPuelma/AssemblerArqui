0a82ead88f01f25d071a9f0be8a41b2a528a851b
Esteban Dib Puelma <edib@uc.cl>
2014-10-24 17:28:28 -0300

    Funcionalidad de CODE: lista, solo falta agregar los labels y la parte de DATA:

--- WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/MainWindow.xaml.cs
@@ -47,7 +47,7 @@ namespace WpfApplication1
                 string filename = dlg.FileName;
                 generateCPUInstructions(filename);
             }
-            generateOutput()
+            generateOutput();
         }
         private void generateCPUInstructions(string file)
         {
@@ -95,33 +95,61 @@ namespace WpfApplication1
             {
                 //TODO: codigo para guardar en memoria el label
             }
+            else if (code.Equals("")) //es un espacio en blanco, no se hace nada
+            {
+            }
             else // ES UNA INSTRUCCIÓN
             {
-                int validador = 0;
                 List<char> codeList = code.ToList<char>();
                 codeList.RemoveAll(isWhiteSpace);
-                string newCode = codeList.ToString();
-                string[] verificador = newCode.Split(',');
+                string newCode = getStringFromList(codeList);
                 string litOrDir = "";
-                if (verificador[1].StartsWith("(") && verificador[1].ElementAt(1).GetType().IsInstanceOfType(validador)) // El string despues de la coma es una dirección de memoria
+                if (newCode.Contains(","))
                 {
-                    litOrDir = getBinaryValue(verificador[1]);
-                    putInstructionInAssembler(verificador[0] + ",(DIR)", litOrDir); //AÑADIR LA DIRECCION, EL PROBLEMA ES QUE PUEDE SER BINARIO, DECIMAL O HEXA
+                    string[] verificador = newCode.Split(',');
+                    if (verificador[1].StartsWith("("))
+                    {
+                        if (Char.IsNumber(verificador[1].ElementAt(1)) || verificador[1].ElementAt(verificador[1].Length - 2) == 'h') // El string despues de la coma es una dirección de memoria 
+                        {
+                            litOrDir = getBinaryValue(verificador[1]);
+                        }
+                        else // el string despues de la coma es una variable
+                        {
+                            //litOrDir = hashDeValores.tryGet("verificador[1].substring(1,verificador[1].length - 2);
+                        }
+                        putInstructionInAssembler(verificador[0] + ",(DIR)", litOrDir);
+                    }
+                    else if (verificador[0].EndsWith(")") && (Char.IsNumber(verificador[0].ElementAt(verificador[0].Length - 2))
+                            || verificador[0].ElementAt(verificador[0].Length - 2) == 'h' || verificador[0].ElementAt(verificador[0].Length - 2) == 'b')) // El string antes de la coma es una dirección de memoria
+                    {
+                        string[] direction = verificador[0].Split('(');
+                        litOrDir = getBinaryValue("(" + direction[1]);
+                        putInstructionInAssembler(direction[0] + "(DIR)," + verificador[1], litOrDir);
+                    }
+                    else if (Char.IsNumber(verificador[1].ElementAt(0)) || verificador[1].ElementAt(verificador[1].Length - 1) == 'h') // El string despues de la coma es un literal
+                    {
+                        litOrDir = getBinaryValue("(" + verificador[1] + ")");
+                        putInstructionInAssembler(verificador[0] + ",LIT", litOrDir);
+                    }
+                    else if (verificador[1].ElementAt(0) != 'A' && verificador[1].ElementAt(0) != 'B') //es un arreglo
+                    {
+                        //litOrDir = hashDeValores.tryGet(verificador[1]);
+                        putInstructionInAssembler(verificador[0] + ",(DIR)", litOrDir);
+                    }
+                    else //Es instrucción cualquiera
+                    {
+                        putInstructionInAssembler(newCode, null); //Mapea la instruccion con su opcode, lo concatena con Mem[... y además lo guarda en assembledLines
+                    }
                 }
-                else if (verificador[0].EndsWith(")") && verificador[0].ElementAt(verificador[0].Length-2).GetType().IsInstanceOfType(validador)) // El string antes de la coma es una dirección de memoria
+                else if (newCode.Contains("("))
                 {
-                    string[] direction = verificador[0].Split('(');
-                    litOrDir = getBinaryValue("(" + direction[1]);
-                    putInstructionInAssembler(direction[0] + "(DIR)," + verificador[1], litOrDir);
+                    string[] verificador = newCode.Split('(');
+                    litOrDir = getBinaryValue("(" + verificador[1]);
+                    putInstructionInAssembler(verificador[0] + "(DIR)", litOrDir);
                 }
-                else if (verificador[1].ElementAt(0).GetType().IsInstanceOfType(validador)) // El string despues de la coma es un literal
+                else
                 {
-                    litOrDir = getBinaryValue("(" + verificador[1] + ")");
-                    putInstructionInAssembler(verificador[0] + ",LIT", litOrDir);
-                }
-                else //Es instrucción cualquiera
-                {
-                    putInstructionInAssembler(newCode, null); //Mapea la instruccion con su opcode, lo concatena con Mem[... y además lo guarda en assembledLines
+                    putInstructionInAssembler(newCode, null);
                 }
             }
         }
@@ -129,7 +157,7 @@ namespace WpfApplication1
         {
             if (number.ElementAt(number.Length - 2) == 'b') //es binario
             {
-                return number.Substring(1, number.Length - 3);
+                return addMissingCerosToBinary(number.Substring(1, number.Length - 3));
             }
             else if (number.ElementAt(number.Length - 2) == 'h') //es hexa
             {
@@ -171,7 +199,7 @@ namespace WpfApplication1
             assembledLines.Add("always 0 (address)");
             assembledLines.Add("begin");
         }
-        private void addFooterInstructions(int lastInstructionNumber)
+        private void addFooterInstructions()
         {
             while(contadorGlobal <= 255)
             {
@@ -187,50 +215,106 @@ namespace WpfApplication1
         private void createHash()
         {
             hash = new Dictionary<string, string>();
-            hash.Add("MOVA,B", "101110000");
-            hash.Add("MOVB,A", "010011000");
-            hash.Add("MOVA,LIT", "101100000");
-            hash.Add("MOVB,LIT", "011100000");
-            hash.Add("MOVA,DIR", "101101000");
-            hash.Add("MOVB,DIR", "011101000");
-            hash.Add("MOVDIR,A", "000011001");
-            hash.Add("MOVDIR,B", "001110001");
-            hash.Add("ADDA,B", "100010000");
-            hash.Add("ADDB,A", "010010000");
-            hash.Add("ADDA,LIT", "100000000");
-            hash.Add("ADDB,LIT", "010000000");
-            hash.Add("SUBA,B", "100010010");
-            hash.Add("SUBB,A", "010010010");
-            hash.Add("SUBA,LIT", "100000010");
-            hash.Add("SUBB,LIT", "010000010");
-            hash.Add("INCB", "011010000");
-            hash.Add("JEQINS", "000010110");
-            hash.Add("JMPINS", "000010100");
+            hash.Add("MOVA,B", "0101110000");
+            hash.Add("MOVB,A", "0010011000");
+            hash.Add("MOVA,LIT", "0101100000");
+            hash.Add("MOVB,LIT", "0011100000");
+            hash.Add("MOVA,(DIR)", "0101101000");
+            hash.Add("MOVB,(DIR)", "0011101000");
+            hash.Add("MOV(DIR),A", "0000011001");
+            hash.Add("MOV(DIR),B", "0001110001");
+            hash.Add("MOVA,(B)", "1101101000");
+            hash.Add("MOV(B),A", "1000011001");
+            hash.Add("ADDA,B", "0100010000");
+            hash.Add("ADDB,A", "0010010000");
+            hash.Add("ADDA,LIT", "0100000000");
+            hash.Add("ADDB,LIT", "0010000000");
+            hash.Add("ADDA,(DIR)", "0100001000");
+            hash.Add("ADDA,(B)", "1100001000");
+            hash.Add("ADD(DIR)", "0000010001");
+            hash.Add("SUBA,B", "0100010010");
+            hash.Add("SUBB,A", "0010010010");
+            hash.Add("SUBA,LIT", "0100000010");
+            hash.Add("SUBB,LIT", "0010000010");
+            hash.Add("SUBA,(DIR)", "0100001010");
+            hash.Add("SUBA,(B)", "1100001010");
+            hash.Add("SUB(DIR)", "0000010011");
+            hash.Add("INCA", "0100000000");
+            hash.Add("INCB", "0011010000");
+            hash.Add("DECA", "0100000010");
+            hash.Add("JEQINS", "0000010110");
+            hash.Add("JMPINS", "0000010100");
+            hash.Add("JNEINS", "1000010110");
         }
         private void putInstructionInAssembler(string instruction, string literal) //instrucción dentro del hash y literal debe ser binario de 5 digitos, por ejemplo "10010" y se refiere a literal o direccion de memoria
         {
             if (literal == null)
             {
-                literal = "00000";
+                literal = "00000000";
             }
-            string start = "Mem[" + contadorGlobal + "] = 25'b00000000";
+            string start = "Mem[" + contadorGlobal + "] = 25'b0000000";
+            contadorGlobal++;
             string result = "";
             hash.TryGetValue(instruction, out result);
-            result = start + result + "000" + literal +";";
+            result = start + result + literal +";";
             assembledLines.Add(result);
         }
         private string hexConverter(string hexvalue)
         {
-            return Convert.ToString(Convert.ToInt32(hexvalue, 16), 2);
+            return addMissingCerosToBinary(Convert.ToString(Convert.ToInt32(hexvalue, 16), 2));
 
         }
         private string decimalConverter(string dec)
         {
-            return Convert.ToString(Int32.Parse(dec), 2);
+            return addMissingCerosToBinary(Convert.ToString(Int32.Parse(dec), 2));
+        }
+
+        private string addMissingCerosToBinary(string inicialBinary)
+        {
+            string completeBinary = "";
+            switch (inicialBinary.Length)
+            {
+                case 1:
+                    completeBinary = "0000000" + inicialBinary;
+                    break;
+                case 2:
+                    completeBinary = "000000" + inicialBinary;
+                    break;
+                case 3:
+                    completeBinary = "00000" + inicialBinary;
+                    break;
+                case 4:
+                    completeBinary = "0000" + inicialBinary;
+                    break;
+                case 5:
+                    completeBinary = "000" + inicialBinary;
+                    break;
+                case 6:
+                    completeBinary = "00" + inicialBinary;
+                    break;
+                case 7:
+                    completeBinary = "0" + inicialBinary;
+                    break;
+                case 8:
+                    completeBinary = inicialBinary;
+                    break;
+            }
+            return completeBinary;
         }
         private void generateOutput()
         {
+            addFooterInstructions();
+            string hola = "";
+        }
 
+        private string getStringFromList(List<char> list)
+        {
+            string converted = "";
+            foreach (char c in list)
+            {
+                converted += c;
+            }
+            return converted;
         }
     }
 }
